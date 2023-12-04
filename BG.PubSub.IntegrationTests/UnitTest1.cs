c2b9257992569b3ad8e80e9a1c84a518081b0606
Nelson Da Silva Neto <nelson.neto@stone.com.br>
2023-12-04 17:27:17 -0300

    Inclusao de AutoFixture

--- BG.PubSub.IntegrationTests/UnitTest1.cs
+++ BG.PubSub.IntegrationTests/UnitTest1.cs
@@ -1,12 +1,15 @@
 // namespace BG.PubSub.IntegrationTests;
 
+using AutoFixture;
+using AutoFixture.AutoMoq;
+
 public class UnitTest1
 {
     [Fact]
     public async void Test1()
     {
-        // var fixture = new Fixture().Customize(new AutoMoqCustomization());
-        // //Arrange
+        var fixture = new Fixture().Customize(new AutoMoqCustomization());
+        //Arrange
         // var evento = fixture.Create<CriaAlunoEvent>();
         // await RabbitFixture.Produce(evento);
         // //Act
