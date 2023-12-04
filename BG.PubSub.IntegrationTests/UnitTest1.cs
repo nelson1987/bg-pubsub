c0d37a11ea605af3877e439dcd694f99fa372552
Nelson Neto <nelson.ash@outlook.com>
2023-12-04 20:30:39 +0000

    Inclusao de Fluent Docker

--- BG.PubSub.IntegrationTests/UnitTest1.cs
+++ BG.PubSub.IntegrationTests/UnitTest1.cs
@@ -2,7 +2,11 @@
 
 using AutoFixture;
 using AutoFixture.AutoMoq;
-
+using Ductus.FluentDocker.Model.Compose;
+using Ductus.FluentDocker.Services;
+using Ductus.FluentDocker.Services.Impl;
+using Ductus.FluentDocker.Model.Common;
+using Microsoft.Extensions.DependencyInjection;
 public class UnitTest1
 {
     [Fact]
@@ -17,5 +21,98 @@ public class UnitTest1
         // // Assert
         // var criaAlunoEvent = RabbitFixture
         //     .Consume<CriaAlunoEvent>();
+    }
+}
+
+public abstract class DockerComposeTestBase : IDisposable
+{
+    protected ICompositeService CompositeService;
+    protected IHostService? DockerHost;
+
+    public DockerComposeTestBase()
+    {
+        EnsureDockerHost();
+
+        CompositeService = Build();
+        try
+        {
+            CompositeService.Start();
+        }
+        catch
+        {
+            CompositeService.Dispose();
+            throw;
+        }
+
+        OnContainerInitialized();
+    }
+
+    public void Dispose()
+    {
+        OnContainerTearDown();
+        var compositeService = CompositeService;
+        CompositeService = null!;
+        try
+        {
+            compositeService?.Dispose();
+        }
+        catch
+        {
+            // ignored
+        }
+    }
+
+    protected abstract ICompositeService Build();
+
+    protected virtual void OnContainerTearDown()
+    {
+    }
+
+    protected virtual void OnContainerInitialized()
+    {
+    }
+
+    private void EnsureDockerHost()
+    {
+        if (DockerHost?.State == ServiceRunningState.Running) return;
+
+        var hosts = new Hosts().Discover();
+        DockerHost = hosts.FirstOrDefault(x => x.IsNative) ?? hosts.FirstOrDefault(x => x.Name == "default");
+
+        if (null != DockerHost)
+        {
+            if (DockerHost.State != ServiceRunningState.Running) DockerHost.Start();
+
+            return;
+        }
+
+        if (hosts.Count > 0) DockerHost = hosts.First();
+
+        if (null != DockerHost) return;
+
+        EnsureDockerHost();
+    }
+}
+public class MyTestFixture : DockerComposeTestBase
+{
+    public MyTestFixture()
+    {
+
+    }
+
+    protected override ICompositeService Build()
+    {
+        var file = Path.Combine(Directory.GetCurrentDirectory(),
+            (TemplateString)"Fixture/docker-compose.yml");
+
+        return new DockerComposeCompositeService(
+            DockerHost,
+            new DockerComposeConfig
+            {
+                ComposeFilePath = new List<string> { file },
+                ForceRecreate = true,
+                RemoveOrphans = true,
+                StopOnDispose = true
+            });
     }
 }
