e585e9d8d2fe6b9ed370c1fc7bd9e08468fad25d
Nelson Neto <nelson.ash@outlook.com>
2023-12-10 08:05:23 -0300

    events

--- BG.PubSub.Api/Program.cs
+++ BG.PubSub.Api/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddTransient<ICriaAlunoProducer, CriaAlunoProducer>();
+builder.Services.AddTransient<ICommandHandler<CriaAlunoCommand>, CriaAlunoCommandHandler>();
 builder.Services.AddMassTransit(x =>
 {
     x.SetKebabCaseEndpointNameFormatter();
@@ -23,10 +24,11 @@ app.UseSwaggerUI();
 app.UseHttpsRedirection();
 
 app.MapPost("/evento", async (string nome,
-    [FromServices] ICriaAlunoProducer producer) =>
+    CancellationToken cancellationToken,
+    [FromServices] ICommandHandler<CriaAlunoCommand> producer) =>
 {
-    var evento = new CriaAlunoEvent() { Nome = nome };
-    await producer.Send(evento);
+    var evento = new CriaAlunoCommand() { Nome = nome };
+    await producer.Handle(evento, cancellationToken);
 })
 .WithName("PublicaEvento")
 .WithOpenApi();
@@ -36,6 +38,39 @@ app.Run();
 public interface ICriaAlunoProducer
 {
     Task Send(CriaAlunoEvent @event);
+}
+public interface ICommand
+{
+}
+public interface ICommandHandler<TCommand> where TCommand : ICommand
+{
+    Task Handle(TCommand command, CancellationToken cancellationToken);
+}
+public interface IAlunoRepository
+{
+    Task Insert(CriaAlunoEvent @event);
+}
+
+public class CriaAlunoCommand : ICommand
+{
+    public Guid Id = Guid.NewGuid();
+    public required string Nome { get; set; }
+}
+
+public class CriaAlunoCommandHandler : ICommandHandler<CriaAlunoCommand>
+{
+    private readonly ICriaAlunoProducer _producer;
+
+    public CriaAlunoCommandHandler(ICriaAlunoProducer producer)
+    {
+        _producer = producer;
+    }
+
+    public async Task Handle(CriaAlunoCommand command, CancellationToken cancellationToken)
+    {
+        var evento = new CriaAlunoEvent() { Nome = command.Nome };
+        await _producer.Send(evento);
+    }
 }
 
 public class CriaAlunoProducer : ICriaAlunoProducer
@@ -57,19 +92,20 @@ public class CriaAlunoProducer : ICriaAlunoProducer
     }
 }
 
-public class CriaAlunoConsumer :
-    IConsumer<CriaAlunoEvent>
+public class CriaAlunoConsumer : IConsumer<CriaAlunoEvent>
 {
     private readonly ILogger<CriaAlunoConsumer> _logger;
-
-    public CriaAlunoConsumer(ILogger<CriaAlunoConsumer> logger)
+    private readonly IAlunoRepository _repository;
+    public CriaAlunoConsumer(ILogger<CriaAlunoConsumer> logger, IAlunoRepository repository)
     {
         _logger = logger;
+        _repository = repository;
     }
 
     public async Task Consume(ConsumeContext<CriaAlunoEvent> context)
     {
         var message = context.Message;
+        await _repository.Insert(message);
         await Console.Out.WriteLineAsync($"Message from Producer : {message.Nome}");
         _logger.LogInformation($"Mensagem Consumida {message}.");
     }
