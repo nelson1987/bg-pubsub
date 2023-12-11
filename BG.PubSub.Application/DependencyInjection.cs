9d3fc10fbcf3ac6f10e8a97be9d17ce387713627
Nelson Da Silva Neto <nelson.neto@stone.com.br>
2023-12-11 15:53:06 -0300

    inclusao de RabbitMq

--- BG.PubSub.Application/DependencyInjection.cs
+++ BG.PubSub.Application/DependencyInjection.cs
@@ -19,13 +19,18 @@ namespace BG.PubSub.Application
             services.AddTransient<ICriaAlunoProducer, CriaAlunoProducer>();
             services.AddTransient<ICommandHandler<CriaAlunoCommand>, CriaAlunoCommandHandler>();
             services.AddTransient<IEventHandler<CriaAlunoEvent>, CriaAlunoEventHandler>();
+
             services.AddMassTransit(x =>
             {
                 x.SetKebabCaseEndpointNameFormatter();
                 x.AddConsumer<CriaAlunoConsumer>();
                 x.UsingRabbitMq((ctx, cfg) =>
                 {
-                    cfg.Host("amqp://guest:guest@localhost:5672");
+                    cfg.Host("localhost", 5672, "vhost", "local", (x) =>
+                    {
+                        x.Username("guest");
+                        x.Password("guest");
+                    });
                     cfg.ConfigureEndpoints(ctx);
                 });
             });
