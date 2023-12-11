a968d5b62c85064c147e6af3e0ad09b3e2c10c29
Nelson Da Silva Neto <nelson.neto@stone.com.br>
2023-12-11 15:01:05 -0300

    criacao de event

---
+++ BG.PubSub.Application/Features/CriaAlunoEvent.cs
@@ -0,0 +1,9 @@
+﻿using BG.PubSub.Application.Abstractions;
+
+namespace BG.PubSub.Application.Features
+{
+    public class CriaAlunoEvent : IEvent
+    {
+        public required string Nome { get; set; }
+    }
+}
